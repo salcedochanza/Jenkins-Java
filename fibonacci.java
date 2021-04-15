cdd8fa342d26b35cd44e0dc6d4827da91e1b4ded
salcedochanza <72934619+salcedochanza@users.noreply.github.com>
2021-04-15 18:36:14 +0200

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,16 @@
+public class fibonacci {
+	 
+    public static void main(String[] args) {
+ 
+        int serie = 10, num1 = 0, num2 = 1, suma = 1;
+        System.out.println(num1);
+         
+        for (int i = 1; i < serie; i++) {
+            System.out.println(suma);
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+        }
+    }
+ 
+}
